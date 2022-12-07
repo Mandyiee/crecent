74f1988512516e85cedb234832c0de70c3ce1044
Mandyiee <chiomzibae101@gmail.com>
2022-12-07 11:32:06 +0100

    deploying to railway

--- crecent/settings.py
+++ crecent/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ze^r2!@@-0pm&m2vf1o5!gao+tiho!+x$3w#7by)l%ds2p89_d'
+SECRET_KEY = env(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
