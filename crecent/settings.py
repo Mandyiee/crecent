af82e83acf6568d57e3e9b7b309a58fa84149f42
Mandyiee <chiomzibae101@gmail.com>
2022-12-08 04:12:55 +0100

    deploying to digital ocean

--- crecent/settings.py
+++ crecent/settings.py
@@ -26,12 +26,12 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
@@ -191,9 +191,9 @@ RECIPIENT_ADDRESS = os.getenv('RECIPIENT_ADDRESS')
 
 #for 587 port use 'django.core.mail.backends.smtp.EmailBackend' 
 
-CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
-SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 
 
 # DATABASE_URL = os.getenv('DATABASE_URL')
